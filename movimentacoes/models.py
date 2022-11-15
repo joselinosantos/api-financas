59208c9bc157b8d173b2dcdf6ba90b00784ce4bc
Joselino Santos <joselinosantosti@gmail.com>
2022-11-15 10:51:32 -0300

    Adicionado campo categoria
    Melhorar essa categoria por receitas e despesas

--- movimentacoes/models.py
+++ movimentacoes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from uuid import uuid4
 
+CATEGORIAS = (('1', 'Casa'), ('2', 'Transporte'), ('3', 'Alimentação'), ('4', 'Saúde'), ('5', 'Vestuário'), ('6', 'Higiene'), ('7', 'Judicial'))
 class Movimentacao(models.Model):
     id_movimentacao = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     descricao = models.CharField(max_length=255)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     tipo = models.CharField(max_length=1, choices=(('e', 'Entrada'), ('s', 'Saída')))
+    categoria = models.CharField(max_length=1, choices=CATEGORIAS)
     data = models.DateField()
     create_at = models.DateField(auto_now_add=True)
